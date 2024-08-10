cd11ee685c427dd0b4c5296d540fd82aff8f897c
InDieTasten <indietasten@gmail.com>
2024-08-11 00:00:38 +0200

    Add easing function for improved gradient visual

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ See higher res images here
 
 | Image | Resolution | Render Time | Size |
 | --- | --- | --- | --- |
-| [mandel-900x600.bmp](https://github.com/InDieTasten/mandel-lua/raw/main/docs/images/mandel-900x600.bmp) | 900x600 | 0.18s | 1.55 MB |
-| [mandel-3000x2000.bmp](https://github.com/InDieTasten/mandel-lua/raw/main/docs/images/mandel-3000x2000.bmp) | 3000x2000 | 2.01s | 17.2 MB |
-| [mandel-9000x6000.bmp](https://github.com/InDieTasten/mandel-lua/raw/main/docs/images/mandel-9000x6000.bmp) | 9000x6000 | 43.3s | 154 MB |
+| [mandel-900x600.bmp](https://github.com/InDieTasten/mandel-lua/raw/main/docs/images/mandel-900x600.bmp) | 900x600 | 0.24s | 1.55 MB |
+| [mandel-3000x2000.bmp](https://github.com/InDieTasten/mandel-lua/raw/main/docs/images/mandel-3000x2000.bmp) | 3000x2000 | 2.83s | 17.2 MB |
+| [mandel-9000x6000.bmp](https://github.com/InDieTasten/mandel-lua/raw/main/docs/images/mandel-9000x6000.bmp) | 9000x6000 | 47.99s | 154 MB |
 
--- main.lua
+++ main.lua
@@ -3,14 +3,25 @@ local Bitmap = require("lib/lua-bitmap")
 
 local path = "docs/images/"
 
+local args = {...}
+
 --output dimensions
-local width = 900
-local height = 600
+local width = tonumber(args[1] or 900)
+local height = tonumber(args[2] or 600)
 
 --complex range to sample
 local topLeft = cn.new(-2, 1)
 local bottomRight = cn.new(1, -1)
 
+local maxIterations = 255
+
+function easingFunction(x)
+    if x >= 1 then
+        return 1
+    else
+        return 1 - 2 ^ (-(maxIterations / 40) * x)
+    end
+end
 
 local filePath = path.."mandel-"..width.."x"..height..".bmp"
 local bmp = Bitmap.empty_bitmap(width, height, false)
@@ -25,16 +36,19 @@ for y = 0, height-1 do
         local c = cn.new(realCoordinate, imaginaryCoordinate)
         local z = cn.new(0, 0)
         local i = 0
-        while i < 100 and cn.mag(z) < 1000000000000 do
+        while i < maxIterations and cn.mag(z) < 1000000000000 do
             z = cn.add(cn.pow(z, 2), c)
             i = i + 1
         end
 
-        local grayScaleValue = math.floor(i/100*255)
+        local ni = i/maxIterations
+
+        local grayScaleValue = easingFunction(ni) * 255
         bmp:set_pixel(x, y, grayScaleValue, grayScaleValue, grayScaleValue)
     end
     print("Progress: "..(math.floor(y/height*10000)/100).."%")
 end
+
 print("Calculations done, writing to file...")
 io.open(filePath, "w"):write(bmp:tostring())
 print("Done writing image to "..filePath)
